904a3fc8058407ecf37ea0554f45385fdd9098af
svenvschie <129615435+svenvschie@users.noreply.github.com>
2024-10-01 11:13:40 +0200

    Create ledstriptest.ino

---
+++ ledstriptest/ledstriptest.ino
@@ -0,0 +1,27 @@
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+#define PIN        6
+#define NUMPIXELS 461
+
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+  clock_prescale_set(clock_div_1);
+#endif
+
+  pixels.begin();
+
+  pixels.clear();
+
+  for(int i=0; i<NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(0, 255, 0));
+    pixels.show();
+  }
+}
+
+void loop() {
+
+}
