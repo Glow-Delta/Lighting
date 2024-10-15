70dc45981414997798f26d0e0d9f5b21ced4d8b5
svenvschie <502539@student.fontys.nl>
2024-10-15 20:36:46 +0200

    Update ledstriptest.ino

--- ledstriptest/ledstriptest.ino
+++ ledstriptest/ledstriptest.ino
@@ -14,19 +14,21 @@ int section3[] = {232, 345}; // terceiro
 int section4[] = {346, 461}; // quarto (paralelo de novo)
 
 void setup() {
-#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
-  clock_prescale_set(clock_div_1);
-#endif
+  #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+    clock_prescale_set(clock_div_1);
+  #endif
 
   pixels.begin();
   pixels.clear();
+
+  setTubeColor(pixels.Color(255, 0, 0)); // Red
+  delay(5000);
 }
 
 void loop() {
   // testHeartbeat();
-  //setTubeColor(pixels.Color(255, 0, 0));
+  
   //givingLife1();
-  pixelConcept(pixels.Color(255, 0, 200));
 }
 
 void setSectionColor(int section[], uint32_t color) {
@@ -41,126 +43,4 @@ void setTubeColor(uint32_t color) {
   setSectionColor(section2, color);  
   setSectionColor(section3, color);  
   setSectionColor(section4, color);
-}
-
-// TEST ANIMATION
-
-void StartUpAnimation(){
-  testAnimation(pixels.Color(0, 255, 0));
-  testAnimation(pixels.Color(255, 0, 0));
-  testAnimation(pixels.Color(0, 0, 255));
-}
-
-void pixelConcept(uint32_t color) {
-  setTubeColor(pixels.Color(0, 0, 30));
-  int maxSteps = section1[1] - section1[0];
-  for (int step = 0; step <= maxSteps + 40 ; step++) {
-    if (section1[0] + step <= section1[1]) {
-      pixels.setPixelColor(section1[0] + step, color);
-      pixels.setPixelColor(section1[0] + step - 20, pixels.Color(0, 0, 30));
-    }
-
-    //if (section2[0] + step <= section2[1]) {
-    //  pixels.setPixelColor(section2[1] - step + 30, pixels.Color(0, 0, 30));
-    //  pixels.setPixelColor(section2[1] - step + 30, pixels.Color(255, 0, 0));
-    //}
-
-
-    pixels.show();
-  }
-}
-
-void testAnimation(uint32_t color) {
-  int maxSteps = max(max(section1[1] - section1[0], section2[1] - section2[0]),
-                     max(section3[1] - section3[0], section4[1] - section4[0]));
-
-  for (int step = 0; step <= maxSteps; step++) {
-    if (section1[0] + step <= section1[1]) {
-      pixels.setPixelColor(section1[0] + step, color);
-    }
-    
-    if (section2[1] - step >= section2[0]) {
-      pixels.setPixelColor(section2[1] - step, color);
-    }
-    
-    if (section3[0] + step <= section3[1]) {
-      pixels.setPixelColor(section3[0] + step, color);
-    }
-    
-    if (section4[1] - step >= section4[0]) {
-      pixels.setPixelColor(section4[1] - step, color);
-    }
-
-    pixels.show();
-  }
-}
-
-
-void rainbowLoop() {
-  int discospeed = 50;
-
-  // Red to Yellow (increase green)
-  for (int i = 0; i <= 255; i+=discospeed) {
-    setTubeColor(pixels.Color(255, i, 0));
-  }
-
-  // Yellow to Green (decrease red)
-  for (int i = 255; i >= 0; i-=discospeed) {
-    setTubeColor(pixels.Color(i, 255, 0));
-  }
-
-  // Green to Cyan (increase blue)
-  for (int i = 0; i <= 255; i+=discospeed) {
-    setTubeColor(pixels.Color(0, 255, i));
-  }
-
-  // Cyan to Blue (decrease green)
-  for (int i = 255; i >= 0; i-=discospeed) {
-    setTubeColor(pixels.Color(0, i, 255));
-  }
-
-  // Blue to Magenta (increase red)
-  for (int i = 0; i <= 255; i+=discospeed) {
-    setTubeColor(pixels.Color(i, 0, 255));
-  }
-
-  // Magenta to Red (decrease blue)
-  for (int i = 255; i >= 0; i-=discospeed) {
-    setTubeColor(pixels.Color(255, 0, i));
-  }
-}
-
-
-// HEART BEAT ANIMATION TEST
-
-void testHeartbeat() {
-  for (int i = 0; i <= 255; i+=5) {
-    setTubeColor(pixels.Color(i, i, 189));
-  }
-
-  for (int i = 255; i > 0; i-=5) {
-    setTubeColor(pixels.Color(i, i, 189));
-  }
-
-  
-}
-
-// giving the pillar life - yellow to orange transition
-
-void givingLife1() {
-   for (int i = 0; i <= 100; i+=5) {
-    setTubeColor(pixels.Color(255, 0 + i, 0));
-  }
-
-  for (int i = 0; i <= 100; i+=5) {
-    setTubeColor(pixels.Color(255, 100 - i, 0));
-  }
-
-     for (int i = 0; i <= 100; i+=25) {
-    setTubeColor(pixels.Color(255, 0 + i, 0));
-  }
-
-  for (int i = 0; i <= 100; i+=25) {
-    setTubeColor(pixels.Color(255, 100 - i, 0));
-  }
 }
