d02eed9160ff886f35d462c0ceac2f0d8918a846
svenvschie <502539@student.fontys.nl>
2024-10-17 09:39:33 +0200

    Updated code to use FastLed library

--- ledstriptest/ledstriptest.ino
+++ ledstriptest/ledstriptest.ino
@@ -1,39 +1,30 @@
-#include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
-  #include <avr/power.h>
-#endif
+#include <FastLED.h>
 
 #define PIN        6
 #define NUMPIXELS  461
-
-Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+CRGB leds[NUMPIXELS];
 
 int section1[] = {0, 116};
 int section2[] = {117, 231};
 int section3[] = {232, 345};
-int section4[] = {346, 461};
+int section4[] = {346, 460};
 
 void setup() {
-#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
-  clock_prescale_set(clock_div_1);
-#endif
-
-  pixels.begin();
-  pixels.clear();
+  FastLED.addLeds<NEOPIXEL, PIN>(leds, NUMPIXELS);
 }
 
 void loop() {
-  StartUpAnimation();
+  rainbowLoop();
 }
 
-void setSectionColor(int section[], uint32_t color) {
+void setSectionColor(int section[], CRGB color) {
   for (int i = section[0]; i <= section[1]; i++) {
-    pixels.setPixelColor(i, color);
+    leds[i] = color;
   }
-  pixels.show();
+  FastLED.show();
 }
 
-void setTubeColor(uint32_t color) {
+void setTubeColor(CRGB color) {
   setSectionColor(section1, color); 
   setSectionColor(section2, color);  
   setSectionColor(section3, color);  
@@ -42,68 +33,67 @@ void setTubeColor(uint32_t color) {
 
 // TEST ANIMATION
 
-void StartUpAnimation(){
-  testAnimation(pixels.Color(0, 255, 0));
-  testAnimation(pixels.Color(255, 0, 0));
-  testAnimation(pixels.Color(0, 0, 255));
+void StartUpAnimation() {
+  testAnimation(CRGB::Green);
+  testAnimation(CRGB::Red);
+  testAnimation(CRGB::Blue);
 }
 
-void testAnimation(uint32_t color) {
+void testAnimation(CRGB color) {
   int maxSteps = max(max(section1[1] - section1[0], section2[1] - section2[0]),
                      max(section3[1] - section3[0], section4[1] - section4[0]));
 
   for (int step = 0; step <= maxSteps; step++) {
     if (section1[0] + step <= section1[1]) {
-      pixels.setPixelColor(section1[0] + step, color);
+      leds[section1[0] + step] = color;
     }
     
     if (section2[1] - step >= section2[0]) {
-      pixels.setPixelColor(section2[1] - step, color);
+      leds[section2[1] - step] = color;
     }
     
     if (section3[0] + step <= section3[1]) {
-      pixels.setPixelColor(section3[0] + step, color);
+      leds[section3[0] + step] = color;
     }
     
     if (section4[1] - step >= section4[0]) {
-      pixels.setPixelColor(section4[1] - step, color);
+      leds[section4[1] - step] = color;
     }
 
-    pixels.show();
+    FastLED.show();
   }
 }
-
 
 void rainbowLoop() {
   int discospeed = 50;
 
   // Red to Yellow (increase green)
-  for (int i = 0; i <= 255; i+=discospeed) {
-    setTubeColor(pixels.Color(255, i, 0));
+  for (int i = 0; i <= 255; i += discospeed) {
+    setTubeColor(CRGB(255, i, 0));
   }
 
   // Yellow to Green (decrease red)
-  for (int i = 255; i >= 0; i-=discospeed) {
-    setTubeColor(pixels.Color(i, 255, 0));
+  for (int i = 255; i >= 0; i -= discospeed) {
+    setTubeColor(CRGB(i, 255, 0));
   }
 
   // Green to Cyan (increase blue)
-  for (int i = 0; i <= 255; i+=discospeed) {
-    setTubeColor(pixels.Color(0, 255, i));
+  for (int i = 0; i <= 255; i += discospeed) {
+    setTubeColor(CRGB(0, 255, i));
   }
 
   // Cyan to Blue (decrease green)
-  for (int i = 255; i >= 0; i-=discospeed) {
-    setTubeColor(pixels.Color(0, i, 255));
+  for (int i = 255; i >= 0; i -= discospeed) {
+    setTubeColor(CRGB(0, i, 255));
   }
 
   // Blue to Magenta (increase red)
-  for (int i = 0; i <= 255; i+=discospeed) {
-    setTubeColor(pixels.Color(i, 0, 255));
+  for (int i = 0; i <= 255; i += discospeed) {
+    setTubeColor(CRGB(i, 0, 255));
   }
 
   // Magenta to Red (decrease blue)
-  for (int i = 255; i >= 0; i-=discospeed) {
-    setTubeColor(pixels.Color(255, 0, i));
+  for (int i = 255; i >= 0; i -= discospeed) {
+    setTubeColor(CRGB(255, 0, i));
   }
 }
