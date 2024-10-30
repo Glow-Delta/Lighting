4aa8a8da8662a4e37cf0034ff9b218a564237bde
svenvschie <502539@student.fontys.nl>
2024-10-30 12:55:26 +0100

    FirstStage refactor, ThirdStage animation reversed

--- ledstriptest/ledstriptest.ino
+++ ledstriptest/ledstriptest.ino
@@ -12,7 +12,6 @@ CRGB cyan = CRGB(0, 255, 255);
 CRGB pink = CRGB(252, 3, 94);
 
 unsigned long lightTimer = millis();
-byte greenIntensity = 255;
 
 struct Section {
   int start;
@@ -32,9 +31,20 @@ void setup() {
 }
 
 void loop() {
-  IdleAnimation();
-  SecondStage();
-  FirstStage();
+  int tempIntSensorCount = 2;
+
+  if (tempIntSensorCount == 0) {
+    IdleAnimation();
+  }
+  if (tempIntSensorCount > 1 && tempIntSensorCount < 4) {
+    FirstStage();
+  }
+  if (tempIntSensorCount >= 4 && tempIntSensorCount < 8) {
+    SecondStage();
+  }
+  if (tempIntSensorCount >= 8) {
+    ThirdStage();
+  }
 }
 
 void IdleAnimation() {
@@ -55,26 +65,29 @@ void IdleAnimation() {
 
 void FirstStage() {
   if ((millis() - lightTimer) > 33) {
-
     int maxStep = (sections[0].end - sections[0].start) / 2;
-    greenIntensity = max(greenIntensity - 85, 0);
+    for (int step = maxStep; step >= 0; step--) {
+      for (int s = 0; s < 4; s++) {
+        leds[sections[s].start + step] = CRGB(255, 255, 0);
+        leds[sections[s].end - step] = CRGB(255, 255, 0);
+      }
+      delay(20);
+      FastLED.show();
+    }
 
     for (int step = maxStep; step >= 0; step--) {
       for (int s = 0; s < 4; s++) {
-        leds[sections[s].start + step] = CRGB(255, greenIntensity, 0);
-        leds[sections[s].end - step] = CRGB(255, greenIntensity, 0);
+        leds[sections[s].start + step] = CRGB(0, 0, 255);
+        leds[sections[s].end - step] = CRGB(0, 0, 255);
       }
+      delay(20);
       FastLED.show();
     }
     lightTimer = millis();
-
-    if (greenIntensity == 0) {
-      greenIntensity = 255;
-    }
   }
 }
 
-void SecondStage() {
+void ThirdStage() {
   if ((millis() - lightTimer) > 33) {
     static int shift = 0;
     static unsigned long lastUpdate = 0;
@@ -86,25 +99,26 @@ void SecondStage() {
     for (int s = 0; s < 4; s++) {
       int sectionLength = sections[s].end - sections[s].start + 1;
 
-      for (int pos = 0; pos < sectionLength; pos++) {
+      for (int pos = sectionLength - 1; pos >= 0; pos--) {
         int ledIndex;
 
-        // Calculate the LED index for each section
+        /*
         if (s % 2 == 0) {
-          ledIndex = sections[s].start + pos;
+          ledIndex = sections[s].start + pos;  // Even sections: still from start to end
         } else {
-          ledIndex = sections[s].end - pos;
+          ledIndex = sections[s].end - (sectionLength - 1 - pos);  // Odd sections: now reverse from end to start
         }
+      */
 
-        // Calculate the color for this shifted position in the gradient
+        ledIndex = sections[s].end - pos;
+
         int gradientPos = map((pos + shift) % sectionLength, 0, sectionLength - 1, 0, 255);
         CRGB color;
 
-        // Determine the color based on the position in the gradient
         if (gradientPos < 128) {
-          color = blend(colors[0], colors[1], gradientPos * 2);  // Cyan to Pink
+          color = blend(colors[0], colors[1], gradientPos * 2);
         } else {
-          color = blend(colors[1], colors[2], (gradientPos - 128) * 2);  // Pink to Yellow
+          color = blend(colors[1], colors[2], (gradientPos - 128) * 2);
         }
 
         leds[ledIndex] = color;
@@ -114,4 +128,7 @@ void SecondStage() {
     shift = (shift + animationSpeed) % sections[0].end;
     lightTimer = millis();
   }
+}
+
+void SecondStage() {
 }
